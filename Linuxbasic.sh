87c4c96d319d65e260943ca53f7430798eac2f8e
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-10-18 18:32:03 +0530

    linux commands basics

---
+++ Linuxbasic.sh
@@ -0,0 +1,8 @@
+#use sudo for admin privileges
+set-prompt mongodb #this is used to set the servername for better understanding
+sudo -i # to move to root user
+last # it tells who all users were logged in to system
+dnf #?
+dnf module list
+systemctl enable nginx
+systemctl status nginx
